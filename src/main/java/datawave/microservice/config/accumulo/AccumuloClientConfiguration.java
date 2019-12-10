bc216e7e735c73f87ea5346a6ec7c2cc22842fe9
Ivan Bella <ivan@bella.name>
2019-12-10 16:36:58 -0500

    Updated for accumulo 2.0.0

--- src/main/java/datawave/microservice/config/accumulo/AccumuloClientConfiguration.java
+++ src/main/java/datawave/microservice/config/accumulo/AccumuloClientConfiguration.java
@@ -1,9 +1,9 @@
 package datawave.microservice.config.accumulo;
 
+import org.apache.accumulo.core.client.Accumulo;
+import org.apache.accumulo.core.client.AccumuloClient;
 import org.apache.accumulo.core.client.AccumuloException;
 import org.apache.accumulo.core.client.AccumuloSecurityException;
-import org.apache.accumulo.core.client.Connector;
-import org.apache.accumulo.core.client.ZooKeeperInstance;
 import org.apache.accumulo.core.client.security.tokens.PasswordToken;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -12,8 +12,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 
 /**
- * A {@link Configuration} that produces Accumulo {@link Connector}s for either the warehouse or metrics Accumulo instnace. These two connectors are produced in
- * a {@link Lazy} fashion, so no attempt to produce them will occur unless there is another bean that attempts to inject a connector using either the
+ * A {@link Configuration} that produces Accumulo {@link AccumuloClient}s for either the warehouse or metrics Accumulo instance. These two clients are produced
+ * in a {@link Lazy} fashion, so no attempt to produce them will occur unless there is another bean that attempts to inject a connector using either the
  * "warehouse" or "metrics" {@link Qualifier}. If you wish to use one of these connectors, then your service must produce an {@link AccumuloProperties} using a
  * {@link Qualifier} of either "warehouse" or "metrics".
  */
@@ -24,17 +24,18 @@ public class AccumuloClientConfiguration {
     @Lazy
     @Qualifier("warehouse")
     @ConditionalOnMissingBean
-    public Connector warehouseConnector(@Qualifier("warehouse") AccumuloProperties accumuloProperties) throws AccumuloSecurityException, AccumuloException {
-        ZooKeeperInstance zooKeeperInstance = new ZooKeeperInstance(accumuloProperties.getInstanceName(), accumuloProperties.getZookeepers());
-        return zooKeeperInstance.getConnector(accumuloProperties.getUsername(), new PasswordToken(accumuloProperties.getPassword()));
+    public AccumuloClient warehouseConnector(@Qualifier("warehouse") AccumuloProperties accumuloProperties)
+                    throws AccumuloSecurityException, AccumuloException {
+        return Accumulo.newClient().to(accumuloProperties.getInstanceName(), accumuloProperties.getZookeepers())
+                        .as(accumuloProperties.getUsername(), new PasswordToken(accumuloProperties.getPassword())).build();
     }
     
     @Bean
     @Lazy
     @Qualifier("metrics")
     @ConditionalOnMissingBean
-    public Connector metricsConnector(@Qualifier("metrics") AccumuloProperties accumuloProperties) throws AccumuloSecurityException, AccumuloException {
-        ZooKeeperInstance zooKeeperInstance = new ZooKeeperInstance(accumuloProperties.getInstanceName(), accumuloProperties.getZookeepers());
-        return zooKeeperInstance.getConnector(accumuloProperties.getUsername(), new PasswordToken(accumuloProperties.getPassword()));
+    public AccumuloClient metricsConnector(@Qualifier("metrics") AccumuloProperties accumuloProperties) throws AccumuloSecurityException, AccumuloException {
+        return Accumulo.newClient().to(accumuloProperties.getInstanceName(), accumuloProperties.getZookeepers())
+                        .as(accumuloProperties.getUsername(), new PasswordToken(accumuloProperties.getPassword())).build();
     }
 }
