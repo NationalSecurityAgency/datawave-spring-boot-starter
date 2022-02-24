83e437fa8779a1ced8b51bf1988da0bc1df21cbf
Bill Oley <bill.oley@gmail.com>
2022-02-24 15:10:36 -0500

    Verify correct ProxiedUserDetails primaryUser in an all-server request

--- src/main/java/datawave/microservice/authorization/user/ProxiedUserDetails.java
+++ src/main/java/datawave/microservice/authorization/user/ProxiedUserDetails.java
@@ -10,11 +10,10 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.xml.bind.annotation.XmlRootElement;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
@@ -24,33 +23,69 @@ import java.util.stream.Collectors;
 @XmlRootElement
 public class ProxiedUserDetails implements UserDetails {
     private String username;
-    private Set<DatawaveUser> proxiedUsers = new LinkedHashSet<>();
+    private List<DatawaveUser> proxiedUsers = new ArrayList<>();
     private List<SimpleGrantedAuthority> roles;
     private long creationTime;
     
     @JsonCreator
     public ProxiedUserDetails(@JsonProperty("proxiedUsers") Collection<? extends DatawaveUser> proxiedUsers, @JsonProperty("creationTime") long creationTime) {
         this.proxiedUsers.addAll(proxiedUsers);
-        this.username = this.proxiedUsers.stream().map(DatawaveUser::getName).collect(Collectors.joining(" -> "));
+        this.username = ProxiedUserDetails.orderProxiedUsers(this.proxiedUsers).stream().map(DatawaveUser::getName).collect(Collectors.joining(" -> "));
         this.roles = getPrimaryUser().getRoles().stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
         this.creationTime = creationTime;
     }
     
     public Collection<? extends DatawaveUser> getProxiedUsers() {
-        return Collections.unmodifiableSet(proxiedUsers);
+        return Collections.unmodifiableList(proxiedUsers);
     }
     
     /**
-     * Gets the {@link DatawaveUser} that represents the primary user in this proxied entity. This is the first (and presumably only) proxied principal whose
-     * {@link DatawaveUser#getUserType()} is {@link UserType#USER}. If no proxied principal is a user principal, then the first principal in the list is
-     * returned.
+     * Gets the {@link DatawaveUser} that represents the primary user in this ProxiedUserDetails. If there is only one DatawaveUser, then it is the primaryUser.
+     * If there is more than one DatawaveUser, then the first (and presumably only) DatawaveUser whose {@link DatawaveUser#getUserType()} is
+     * {@link UserType#USER} is the primary user. If no such DatawaveUser is present, then the second principal in the list is returned as the primary user.
+     * This will be the first entity in the X-ProxiedEntitiesChain which should be the server that originated the request.
      *
-     * @return The {@link DatawaveUser} that represents the user in the list of proxied users
+     * @return The {@link DatawaveUser} that represents the primary user in the list of proxied users
      */
     @JsonIgnore
     public DatawaveUser getPrimaryUser() {
-        DatawaveUser first = proxiedUsers.stream().findFirst().orElse(null);
-        return proxiedUsers.stream().filter(u -> u.getUserType() == UserType.USER).findFirst().orElse(first);
+        return ProxiedUserDetails.findPrimaryUser(this.proxiedUsers);
+    }
+    
+    static protected DatawaveUser findPrimaryUser(List<DatawaveUser> datawaveUsers) {
+        if (datawaveUsers.size() <= 1) {
+            return datawaveUsers.get(0);
+        } else {
+            DatawaveUser secondInOrder = datawaveUsers.get(1);
+            return datawaveUsers.stream().filter(u -> u.getUserType() == UserType.USER).findFirst().orElse(secondInOrder);
+        }
+    }
+    
+    /*
+     * The purpose here is to return a List of DatawaveUsers where the original caller is first followed by any entities in X-ProxiedEntitiesChain in the order
+     * that they were traversed and ending with the entity that made the final call. The List that is passed is not modified. This method makes the following
+     * assumptions about the List that is passed to ths method: 1) The first element is the one that made the final call 2) Additional elements (if any) are
+     * from X-ProxiedEntitiesChain in chronological order of the calls
+     */
+    static protected List<DatawaveUser> orderProxiedUsers(List<DatawaveUser> datawaveUsers) {
+        DatawaveUser primary = ProxiedUserDetails.findPrimaryUser(datawaveUsers);
+        List<DatawaveUser> users = new ArrayList<>();
+        users.add(primary);
+        if (datawaveUsers.size() > 1) {
+            // @formatter:off
+            // Skipping first user because if it is UserType.USER, it is the primary
+            // and already added.  If it is UserType.Server, then it will be added at the end
+            users.addAll(datawaveUsers.stream()
+                    .skip(1)
+                    .filter(u -> u != primary)
+                    .collect(Collectors.toList()));
+            // @formatter:on
+            DatawaveUser first = datawaveUsers.get(0);
+            if (!users.contains(first)) {
+                users.add(first);
+            }
+        }
+        return users;
     }
     
     @Override
@@ -79,7 +114,7 @@ public class ProxiedUserDetails implements UserDetails {
         // @formatter:off
         return "ProxiedUserDetails{" +
                 "username='" + username + '\'' +
-                ", proxiedUsers=" + proxiedUsers +
+                ", proxiedUsers=" + ProxiedUserDetails.orderProxiedUsers(proxiedUsers) +
                 '}';
         // @formatter:on
     }
