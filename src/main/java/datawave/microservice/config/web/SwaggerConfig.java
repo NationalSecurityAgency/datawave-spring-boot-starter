45b5c63482dae3cfc3b2abc34eae0ac3cdba50b1
Whitney O'Meara <james.w.omeara@gmail.com>
2022-06-10 15:26:01 -0400

    removed unused spring fox configuration, and added springdoc for swagger api generation

--- src/main/java/datawave/microservice/config/web/SwaggerConfig.java
+++ src/main/java/datawave/microservice/config/web/SwaggerConfig.java
@@ -1,44 +1,25 @@
-// package datawave.microservice.config.web;
-//
-// import com.google.common.base.Predicate;
-// import org.springframework.beans.factory.annotation.Value;
-// import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import springfox.documentation.RequestHandler;
-// import springfox.documentation.builders.PathSelectors;
-// import springfox.documentation.service.ApiInfo;
-// import springfox.documentation.service.Contact;
-// import springfox.documentation.spi.DocumentationType;
-// import springfox.documentation.spring.web.plugins.Docket;
-// import springfox.documentation.swagger2.annotations.EnableSwagger2;
-//
-// import java.util.Arrays;
-//
-/// **
-// * Configuration to generate Swagger documentation for REST endpoints.
-// */
-// @Configuration
-// @EnableSwagger2
-// public class SwaggerConfig {
-// @Bean
-// @ConditionalOnMissingBean
-// public Docket api(@Value("${spring.application.name}") String appName, @Value("${swagger.doc.packages:datawave.microservice}") String[] docPackages) {
-//        // @formatter:off
-//        return new Docket(DocumentationType.SWAGGER_2)
-//            .select()
-//            .apis(basePackages(docPackages))
-//            .paths(PathSelectors.any())
-//            .build()
-//            .apiInfo(apiInfo(appName, SwaggerConfig.class.getPackage().getImplementationVersion()));
-//        // @formatter:on
-// }
-//
-// private ApiInfo apiInfo(String appName, String appVersion) {
-// return new ApiInfo(appName + " API", "REST operations provided by the " + appName + " API", appVersion, null, (Contact) null, null, null);
-// }
-//
-// private static Predicate<RequestHandler> basePackages(String[] packageNames) {
-// return input -> Arrays.stream(packageNames).anyMatch(pkgName -> input.declaringClass().getPackage().getName().startsWith(pkgName));
-// }
-// }
+package datawave.microservice.config.web;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configuration to generate Swagger documentation for REST endpoints.
+ */
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI springDocsOpenAPI(@Value("${spring.application.name}") String appName) {
+        return new OpenAPI()
+                        .info(new Info().title(appName + " API").description("REST operations provided by the " + appName + " API")
+                                        .version(SwaggerConfig.class.getPackage().getImplementationVersion())
+                                        .license(new License().name("Apache License 2.0").url("https://www.apache.org/licenses/LICENSE-2.0")))
+                        .externalDocs(new ExternalDocumentation().description("Additional documentation available on GitHub")
+                                        .url("https://github.com/NationalSecurityAgency/datawave"));
+    }
+}
